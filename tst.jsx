8ed462299dc66eb680ea0216a299bd1914727225
Grant Royster <grant@theroysters.com>
2019-02-18 14:35:54 -0500

    Added functionality to focus on nodes

--- tst.jsx
+++ tst.jsx
@@ -10,19 +10,43 @@ var cy = cytoscape({
 		{ selector: 'node',
 			style: {
 				'background-color': '#666',
-				'label': 'data(id)',
+				'label': 'data(name)',
 				'font-size': 5,
-				'width': 3,
-				'height': 3,
-				'padding': '20%'
+				'width': function (ele) {
+					return 2 + Math.pow(ele.degree(true), 7/12);
+				},
+				'height': function (ele) {
+					return 2 + Math.pow(ele.degree(true), 7/12);
+				},
+				'border-width': 0.5,
+				'padding': '20%',
+				'background-color': function (ele) {
+					if (ele.hasClass('named')) {
+						return 'green';
+					} else {
+						return 'gray';
+					}
+				},
+				'color': function (ele) {
+					if (ele.hasClass('named')) {
+						return 'darkgreen';
+					} else {
+						return 'black';
+					}
+				},
+				'opacity': 0.9			
 			}
 		},
 		{ selector: 'edge',
 	      		style: {
-	        		'width': 2,
-	        		'line-color': '#ccc',
+	        		'width': function(ele){
+					return (0.9 + Math.log2(ele.data('number_interactions')));
+				}, 
+	        		'line-color': '#bbb',
 	        		'target-arrow-color': '#ccc',
-	        		'target-arrow-shape': 'triangle'
+	        		'target-arrow-shape': 'triangle',
+				'opacity': 0.7 
+					
 	      		}
 	    	}
 	  ],
@@ -32,7 +56,7 @@ var cy = cytoscape({
 var setFile = (evt) => {
 	let file = evt.target.files[0];
 	csv_to_json(file).then((json) => {
-		setElements(extractElements(json.data));	
+		setElements(json.data);
 	});
 };
 
@@ -41,7 +65,6 @@ var setFile = (evt) => {
 // NOTE: the JSON is contaiend in response.data
 var csv_to_json = (csv) => {
 	// Using Papa for the CSV => JSON conversion	
-	console.log(csv);	
 	return new Promise(function(complete, error) {
 		Papa.parse(csv, {
 			header: true,
@@ -51,39 +74,137 @@ var csv_to_json = (csv) => {
 };
 
 // Takes JSON extracted from CSV file; formats for Cytoscape
-var extractElements = (json) => {
-	let elements = [];
+var setElements = (json) => {
+
+	// startBatch prevents Cytoscape from rendering elements as we add and change them
+	cy.startBatch();
+	
 	for (let i = 0; i < json.length; i++){
-		let src = json[i]["Bait Locus"];
-		let dst = json[i]["Prey Locus"];
+		let bait_name = json[i]["Bait Name"];
+		let bait_locus = json[i]["Bait Locus"];
+		let bait_notes = json[i]["Bait Notes"];
+		let prey_locus = json[i]["Prey Locus"];
+		let prey_tair = json[i]["Prey TAIR Symbols"];
+		let prey_description = json[i]["Prey TAIR Description"];
+		let number_interactions = json[i]["Number of Interactions"];
 
-		if (src != '' ) {
-			elements.push({	data: {id: src }});
-		};
-				
-		if (dst != '' ) {
-			elements.push({	data: {id: dst}});
-		};
+		if (bait_locus == ''){ break; }
 
-		if ((src != '') && (dst != '')){
-			elements.push({ data: {
-				id: src + "_" + dst,
-				source: src,
-				target: dst
-			}});
+		// Add bait protein node
+		let bait = cy.$id(bait_locus);	
+		if (bait.length == 0) {
+			bait = cy.add({
+				group: 'nodes',
+				data: {
+					id: bait_locus,
+					name: bait_locus,
+				}
+			});
+		}
+
+		// If bait has a name, and if the node hasn't been named, then add one
+		if (!bait.hasClass('named') && (bait_name != "")) {
+			bait.addClass('named');
+			bait.data('name', bait_name);
+		}	
+	
+		// If the node doesn't already have bait notes, add it
+		if (!bait.data('bait_notes') && bait_notes) {
+			bait.data('bait_notes', bait_notes);
+		}
+	
+		// Add prey protein node
+		let prey = cy.$id(prey_locus);
+		if (prey.length == 0) {
+                        prey = cy.add({
+                                group: 'nodes',
+                                data: {
+                                        id: prey_locus,
+					name: prey_locus,
+                                }   
+                        }); 
+                }	
+
+		// If the node doesn't have prey_tair, add it
+		if (!prey.data('prey_tair') && prey_tair) {
+			prey.data('prey_tair', prey_tair);
+		}
+
+		// If the node doesn't have prey_description, add it
+		if (!prey.data('prey_description') && prey_description) {
+			prey.data('prey_description', prey_description);
+		}
+	
+		// Add edge; ignore duplicates
+		let edge_id = bait_locus + '_' + prey_locus;
+		if (cy.$id(edge_id).length == 0) {
+			cy.add({
+				group: 'edges',
+				data: {
+					id: edge_id,
+					source: bait_locus,
+					target: prey_locus,
+					number_interactions: number_interactions,		
+				},
+			});
+		} else {
+			console.log('duplicate entry: ' + edge_id);
 		}
 	}
-	return elements;
+	
+	// 'Cose': a particular built-in layout (for positioning nodes)
+	var layout = cy.layout({ name: 'cose' }); 
+
+	// Layout doesn't affect the graph until it's run
+        layout.run();
+
+	// Fits the screen to the entire collection of nodes	
+	cy.fit();
+
+	// Displays the graph
+	cy.endBatch();
 };
 
-// Sets the Cytoscape elements to the elements[] array
-var setElements = (elements) => {
-	cy.add(elements);
-	var layout = cy.layout({
-		name: 'cose',
-	});
-	layout.run();
-	cy.fit();
-}
-
+// Add event listener for file button
 document.getElementById('files').addEventListener('change', setFile, false);
+
+// Add event listener for the spacebar
+window.addEventListener("keydown", keypress, false);
+
+// If the spacebar is pressed, we re-center to the entire graph
+function keypress(e) {
+	if (e.key == ' ') {
+		cy.fit();
+	}
+};
+
+// Upon clicking a node, hide any edge or node that isn't connected
+cy.on('click', 'node', function(evt){
+	var node = evt.target;
+	console.log(node.data());
+	let neighbors = node.neighborhood();
+	cy.elements().style('opacity', function (ele) {
+		if (ele.isNode()) {
+			return (0.9 / 6);
+		} 
+		return (0.8 / 6);
+	});
+	neighbors.style('opacity', 1);
+	node.style('opacity', 0.9);
+	
+	cy.fit(neighbors);
+
+	// Add a one-use click listener to exit the view
+	cy.once('click', function(evt) {
+		console.log(evt.target.id);
+
+		// We still want to be able to apply the existing functionality to nodes
+		if(!evt.target.id || !evt.target.isNode()) {
+			cy.elements().style('opacity', function(ele) {
+				if (ele.isNode()) {
+					return 0.9
+				} return 0.8;
+			});
+		}
+	});
+});
